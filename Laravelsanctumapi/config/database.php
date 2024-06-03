ba539e33ce7df1dbfdd71bc62bc0cf5e5e4e79ac
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2024-06-03 11:03:31 +0530

    Successfully database migration into phpmyadmin

--- Laravelsanctumapi/config/database.php
+++ Laravelsanctumapi/config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -32,7 +32,7 @@ return [
     'connections' => [
 
         'sqlite' => [
-            'driver' => 'sqlite',
+            'driver' => 'mysql',
             'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # User-Registration-Laravel-11-Sanctum-API-with-Postman
-<h1>Laravel-11-Sanctum-API-with-Postman</h1>
+Laravel-11-Sanctum-API-with-Postman
 
 Laravel Sanctum works as your API bouncer, ensuring only authorized users enter. It prevents the use of clunky OAuth systems and offers a simple, token-based approach. Each user gets theri own unique API token, acting like a secret password to access specific resources within your app.
+
+To run : php artisan serve
 
 
