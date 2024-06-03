0e10eb90eb62572295d885a347548aed54c0a8c2
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2024-06-03 11:43:54 +0530

    AuthControllers were added

--- Laravelsanctumapi/app/Http/Controllers/AuthController.php
+++ Laravelsanctumapi/app/Http/Controllers/AuthController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $data = $request->validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:6']
+        ]);
+
+        $user = User::create($data);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return[
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
 }
